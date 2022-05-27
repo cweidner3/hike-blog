7c020f9efe6746f9671bc2fe0a58982196f1da3d
Cora Weidner <cweidner3+git@gmail.com>
2022-05-27 14:13:08 -0400

    Added map to page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import {useEffect, useRef} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import mapboxgl from 'mapbox-gl';
+
+const {
+  MAPBOXGL_ACCESS_TOKEN = 'pk.eyJ1IjoiY3dlaWRuZXIzIiwiYSI6ImNsM29wbDU0dTBxdjkzY3V2ajk2Y2I3MXcifQ.KYe1u_UcNKFneq-d0bCU0g',
+} = process.env;
+
+
+mapboxgl.accessToken = MAPBOXGL_ACCESS_TOKEN
+
+function mapStyleUrl(name) {
+  if (name === 'outdoors') {
+    return 'mapbox://styles/mapbox/outdoors-v11';
+  }
+  else if (name === 'satellite') {
+    return 'mapbox://styles/mapbox/satellite-v9';
+  }
+  else if (name === 'streets') {
+    return 'mapbox://styles/mapbox/streets-v11';
+  }
+  return 'mapbox://styles/mapbox/outdoors-v11';
+}
+
+
 function App() {
+  const mapStyleName = 'outdoors-v11';
+
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const map = new mapboxgl.Map( {
+      container: mapRef.current,
+      style: mapStyleUrl(mapStyleName),
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }, []);
+
+
+  const mapStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ref={mapRef} className="map-container" style={mapStyle} />
   );
 }
 
 export default App;
+
+// vim: set sw=2 ts=2 expandtab:
