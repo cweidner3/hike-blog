94a70893654a305cf445770b6e8186e57ecc26dc
Cora Weidner <cweidner3+git@gmail.com>
2022-12-10 13:29:47 -0500

    Some changes to better work with mysql/mariadb

--- api/src/migrations/20221204_0800-991b63134bdc_add_timezone_column_to_tables_with_.py
+++ api/src/migrations/20221204_0800-991b63134bdc_add_timezone_column_to_tables_with_.py
@@ -7,7 +7,7 @@ Create Date: 2022-12-04 08:00:49.236126
 """
 from alembic import op
 import pytz
-from sqlalchemy import Column, Date, DateTime, Text, ForeignKey
+from sqlalchemy import Column, Date, DateTime, String, Text, ForeignKey
 
 
 # revision identifiers, used by Alembic.
@@ -20,14 +20,13 @@ depends_on = None
 def upgrade() -> None:
     tztable = op.create_table(
         'timezones',
-        Column('name', Text, index=True, primary_key=True),
+        Column('name', String(256), index=True, primary_key=True),
     )
     assert tztable is not None
     op.bulk_insert(tztable, list(map(lambda x: dict(name=x), pytz.common_timezones)))
 
-    op.add_column('hikes', Column('zone', Text, ForeignKey('timezones.name'), server_default='UTC'))
+    op.add_column('hikes', Column('zone', String(256), ForeignKey('timezones.name'), server_default='UTC'))
     op.execute("UPDATE hikes SET zone = 'UTC'")
-    op.alter_column('hikes', 'zone', nullable=False)
     op.alter_column('hikes', 'start', type_=DateTime)
     op.alter_column('hikes', 'end', type_=DateTime)
 
