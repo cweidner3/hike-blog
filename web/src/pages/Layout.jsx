cfce5b861f38a087e411f7f5969818601b35def1
Cora Weidner <cweidner3+git@gmail.com>
2022-12-12 16:05:11 -0500

    Have docker insert version info into a json file

--- web/src/pages/Layout.jsx
+++ web/src/pages/Layout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../Navbar.jsx';
+import VersionData from '../meta/version.json';
 
 function Layout(props = {}) {
     return (
+        <>
         <div className='container'>
             <Navbar />
             <Outlet />
         </div>
+        <footer className='footer has-background-dark'>
+            <div className='level'>
+                <p className='level-item'>App Version: {VersionData.version}</p>
+            </div>
+        </footer>
+        </>
     );
 }
 
